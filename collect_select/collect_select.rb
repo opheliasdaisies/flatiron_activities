30577b7abf175eeff2c47a045d972a2d2ea30071
Sara Gorecki <sgorecki@gmail.com>
2013-11-20 13:20:42 -0500

    add collect_select.rb file and test file

---
+++ collect_select/collect_select.rb
@@ -0,0 +1,12 @@
+#Implement your own versions of collect and select
+ 
+def my_collect(array)
+#code goes here
+end
+ 
+def my_select(array)
+#
+end
+ 
+#CHALLENGE:
+#Implement these methods as instance methods on the array class.
