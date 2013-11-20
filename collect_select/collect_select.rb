15dc21e62ffaefa4be554de93eb78d74b7b02477
Sara Gorecki <sgorecki@gmail.com>
2013-11-20 13:37:26 -0500

    add code to make a collect method

--- collect_select/collect_select.rb
+++ collect_select/collect_select.rb
@@ -1,7 +1,14 @@
 #Implement your own versions of collect and select
  
 def my_collect(array)
-#code goes here
+	i = 0
+	new_array = []
+	while i < array.length
+		x = yield array[i]
+		new_array << x
+		i += 1
+	end
+	new_array
 end
  
 def my_select(array)
