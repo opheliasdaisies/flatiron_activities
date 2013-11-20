d03576027c48ea53b69eba6e09f65badb7660b25
Sara Gorecki <sgorecki@gmail.com>
2013-11-20 13:50:23 -0500

    add tests to test for defining the methods within the Array class

--- collect_select/collect_select_spec.rb
+++ collect_select/collect_select_spec.rb
@@ -17,3 +17,22 @@ describe "#my_select" do
 		expect(my_select([32,42,35,23,52,52,34,17,69]){|item| item > 35}).to eq([42,52,52,69])
 	end
 end
+
+
+describe Array, "#my_collect" do
+	it "should iterate through an array, do something to each element of the array, and return the new array." do
+		expect([1,2,3,4,5].my_collect {|item| item *2 }).to eq([2,4,6,8,10])
+	end
+	it "should iterate through an array, do something to each element of the array, and return the new array." do
+		expect(["one","two","three"].my_collect {|item| "#{item}, yo!"}).to eq(["one, yo!", "two, yo!", "three, yo!"])
+	end
+end
+
+describe "#my_select" do
+	it "should iterate through an array, and return all elements which meet a given condition." do
+		expect([1,2,3,4,5,6].my_select{|item| item.even?}).to eq([2,4,6])
+	end
+	it "should iterate through an array, and return all elements which meet a given condition." do
+		expect([32,42,35,23,52,52,34,17,69].my_select {|item| item > 35}).to eq([42,52,52,69])
+	end
+end
